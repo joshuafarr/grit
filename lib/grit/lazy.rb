1c36cb45c335e954eb0297082643fafbd80441f5
Joshua Farr <joshua.farr@firstgiving.com>
2011-04-28 10:34:22 -0700

    break the lazy loader to work with older ruby installation

--- lib/grit/lazy.rb
+++ lib/grit/lazy.rb
@@ -24,10 +24,8 @@ module Lazy
     args.each do |arg|
       ivar = "@#{arg}"
       define_method(arg) do
-        if instance_variable_defined?(ivar)
-          val = instance_variable_get(ivar)
-          return val if val
-        end
+        val = instance_variable_get(ivar)
+        return val if val
         instance_variable_set(ivar, (@lazy_source ||= lazy_source).send(arg))
       end
     end
